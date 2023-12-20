7a18c9824c6baf97a8340555ddef8e47331d7001
GewinCodeX <chibuzornworie85@gmail.com>
2023-12-20 21:30:58 +0100

    chore: added missed route

--- src/components/settings/account/Content.tsx
+++ src/components/settings/account/Content.tsx
@@ -57,10 +57,10 @@ export const Content: FC = () => {
     <>
       <div style={{ fontFamily: "Figtree" }} className="lg:w-[780px] w-[90%]">
         <div className="flex justify-end">
-          <NavLink to="/client" className="hidden lg:flex">
+          <NavLink to="/free_client" className="hidden lg:flex">
             <img src={closeIcon} alt={closeIcon} />
           </NavLink>
-          <NavLink to="/client" className="flex lg:hidden w-[32px] h-[32px]">
+          <NavLink to="/free_client" className="flex lg:hidden w-[32px] h-[32px]">
             <img src={closeIcon} alt={closeIcon} />
           </NavLink>
         </div>
